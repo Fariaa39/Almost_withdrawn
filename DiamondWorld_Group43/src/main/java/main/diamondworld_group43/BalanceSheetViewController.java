283713322764203120011dc31e0141c719a2dd90
RaIHaN-dash-25 <raihan.hossain2505@gmail.com>
2024-07-17 21:47:46 +0600

    balance sheet update

---
+++ DiamondWorld_Group43/src/main/java/main/diamondworld_group43/BalanceSheetViewController.java
@@ -0,0 +1,45 @@
+package main.diamondworld_group43;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class BalanceSheetViewController
+{
+    @javafx.fxml.FXML
+    private TableView assetTableView;
+    @javafx.fxml.FXML
+    private TableColumn liabTreasuryStockTableCol;
+    @javafx.fxml.FXML
+    private TableColumn liabTotalTaxPaidTableCol;
+    @javafx.fxml.FXML
+    private TableColumn assetTotalInvestmentTableCol;
+    @javafx.fxml.FXML
+    private TableColumn liabTotalLoanTableCol;
+    @javafx.fxml.FXML
+    private TableColumn liabCurrentTaxPayTableCol;
+    @javafx.fxml.FXML
+    private TableColumn assetAcountRecTableCol;
+    @javafx.fxml.FXML
+    private TableColumn assetPrepaidExpenseTableCol;
+    @javafx.fxml.FXML
+    private TableColumn assetCashEquTableCol;
+    @javafx.fxml.FXML
+    private TableColumn assetMonthYearTableCol;
+    @javafx.fxml.FXML
+    private TableColumn liabCustomerDepTableCol;
+    @javafx.fxml.FXML
+    private TableView liabilityTableView;
+    @javafx.fxml.FXML
+    private TableColumn liabMonthYearTableCol;
+    @javafx.fxml.FXML
+    private TableColumn liabTotalPayTableCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void backDashOnAction(ActionEvent actionEvent) {
+    }
+}
