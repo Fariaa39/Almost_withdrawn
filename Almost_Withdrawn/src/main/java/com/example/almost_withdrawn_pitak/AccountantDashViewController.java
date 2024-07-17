a3a3a0098263ca86bd0c6d9939453dac01952e8a
RaIHaN-dash-25 <raihan.hossain2505@gmail.com>
2024-07-17 22:33:17 +0600

    afafa

---
+++ Almost_Withdrawn/src/main/java/com/example/almost_withdrawn_pitak/AccountantDashViewController.java
@@ -0,0 +1,42 @@
+package main.diamondworld_group43;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+
+public class AccountantDashViewController
+{
+    @javafx.fxml.FXML
+    private ComboBox financialStatementComboBox;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void supplierBillOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void employeeExpenditureOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void financialStatementOptionOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void checkInventoryOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void signOutButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void exchangeBillOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void taxRecordOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ Almost_Withdrawn/src/main/java/com/example/almost_withdrawn_pitak/SalesRepDashViewController.java
@@ -0,0 +1,46 @@
+package main.diamondworld_group43;
+
+import javafx.event.ActionEvent;
+
+public class SalesRepDashViewController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void addNewItemOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void customOrderOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void checkInventoryOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void createSalesReportOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void purchaseDetailsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void paymentMethodOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void currentOffersOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void signOutOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void loyaltyProgramOnAction(ActionEvent actionEvent) {
+    }
+}
